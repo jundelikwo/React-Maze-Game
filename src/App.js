2ecc621cf83185335c145236cbebc32a3839d14a
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2018-12-01 20:30:40 +0100

    Prompt the user for board's width & height

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    let width = prompt("Please enter board width")
+    let height = prompt("Please enter board height")
+    width = (!width || isNaN(width) || width == 0) ? 10 : width
+    height = (!height || isNaN(height) || height == 0) ? 10 : height
+    this.state = { height, width }
+  }
+
   render() {
+    console.log('width: ',this.state.width," height: ",this.state.height)
     return (
       <div className="App">
         <header className="App-header">
